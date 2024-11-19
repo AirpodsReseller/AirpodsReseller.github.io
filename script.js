61e889f3f740b227ca72f5c7b6946ee13541fab3
AirpodsReseller <romanbou6@gmail.com>
2024-11-18 20:57:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const header = document.querySelector('.header');
 
 // Ajouter un événement sur le défilement
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) { // Si le scroll dépasse 50px
+    if (window.scrollY > 500) { // Si le scroll dépasse 50px
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
